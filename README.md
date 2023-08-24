bdf943948c080fc6f5c1426868e2c962e38c7a64
nordinbc <61682191+nordin-lab@users.noreply.github.com>
2023-08-24 17:04:16 +0200

    1ra version

--- README.md
+++ README.md
@@ -2,6 +2,32 @@
  
 ## Damavis 2023
 
-Problema clásico de búsqueda en laberinto.
+### Introducción
+
+Se plantea un problema donde se debe recorrer con una barra de tamaño `1x3` desde la esquina superior izquierda de un laberinto hasta la esquina inferior derecha. La barra puede moverse en las direcciones básicas y rotar sobre su punto medio. Hemos empleado una estrategia BFS para explorar las posibles soluciones, validando cada movimiento y rotación según las reglas establecidas por la actividad.
+
+### Variables 
+
+- `rows` y `cols`: Representan las dimensiones del laberinto. 
+- `visited`: Un conjunto que guarda los nodos que hemos visitado. 
+- `q`: Una cola (`deque`) que contiene los nodos pendientes de procesar. Cada nodo tiene información sobre la posición actual de la barra, su orientación y el número de movimientos realizados para llegar allí.
+
+### Estructuras de Datos Utilizadas
+
+1. **Conjunto (`set`)**: Esencial para operaciones de inserción y búsqueda en tiempo constante. Garantiza que no procesemos el mismo estado múltiples veces.
+2. **Cola (`deque`)**: Proporciona operaciones en los extremos en tiempo constante. Esencial para la estrategia BFS, donde insertamos estados para procesar y los eliminamos una vez procesados.
+
+### Funciones
+
+1. `valid_move`: Verifica si una posición (r, c) dada es válida para una orientación específica de la barra.
+2. `can_rotate`: Determina si es posible rotar la barra en una posición y orientación dadas.
+
+### Estrategia
+
+1. **Evitar procesamiento redundante**: Utilizamos el conjunto `visited` para no procesar un nodo repetidamente. 
+2. **Validaciones tempranas**: Hacemos comprobaciones iniciales, como ver si una posición está fuera de los límites. Estas comprobaciones rápidas previenen cálculos innecesarios.
+3. **Búsqueda BFS**: Usamos Búsqueda en Amplitud porque buscamos el camino más corto en un espacio sin ponderaciones.
+
+---
 
 Realizado por: Nordin El Balima Cordero
---
+++ main.py
@@ -0,0 +1,90 @@
+from collections import deque
+
+def solution(labyrinth):
+    rows, cols = len(labyrinth), len(labyrinth[0])
+
+    def valid_move(r, c, orientation):
+        if orientation == 'H':
+            if c + 2 >= cols or r >= rows or c < 0:  
+                return False
+            return all(labyrinth[r][c + i] == '.' for i in range(3))
+        else:  # orientation == 'V'
+            if r + 2 >= rows or c >= cols or r < 0:  
+                return False
+            return all(labyrinth[r + i][c] == '.' for i in range(3))
+
+    def can_rotate(r, c, orientation):
+        if orientation == 'H':  # Centro de rotación en (r, c+1)
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if 0 <= r+i < rows and 0 <= c+j+1 < cols and labyrinth[r+i][c+j+1] == '#':
+                        return False
+        else:  # orientation == 'V'
+            for i in range(-1, 2):  # Centro de rotación está en (r+1, c)
+                for j in range(-1, 2):
+                    if 0 <= r+i+1 < rows and 0 <= c+j < cols and labyrinth[r+i+1][c+j] == '#':
+                        return False
+        return True
+
+
+    visited = set()
+    q = deque()
+    q.append(((0, 0), 'H', 0))  # posición, orientación, número de movimientos
+
+    while q:
+        (r, c), orientation, moves = q.popleft()
+
+        # Si llegamos a la posición final
+        if (orientation == 'H' and r == rows - 1 and c == cols - 3) or (orientation == 'V' and r == rows - 3 and c == cols - 1):
+            return moves
+
+        # Probamos todos los movimientos
+        for dr, dc, do in [(1, 0, '0'), (-1, 0, '0'), (0, 1, '0'), (0, -1, '0'), (0, 0, 'R')]:
+            nr, nc, no = r + dr, c + dc, orientation if do == '0' else ('H' if orientation == 'V' else 'V')
+
+            # Si las coordenadas están fuera de los límites, continúa al siguiente ciclo
+            if nr < 0 or nc < 0 or nr >= rows or nc >= cols:
+                continue
+
+            # Si es una rotación, verificamos si es posible
+            if do == 'R' and not can_rotate(r, c, orientation):
+                continue
+
+            if (nr, nc, no) not in visited and valid_move(nr, nc, no):
+                visited.add((nr, nc, no))
+                q.append(((nr, nc), no, moves + 1))
+
+    return -1
+
+
+# --------------------------------- TEST ----------------------------------
+
+labyrinths = [
+    [[".",".",".",".",".",".",".",".","."],
+    ["#",".",".",".","#",".",".",".","."],
+    [".",".",".",".","#",".",".",".","."],
+    [".","#",".",".",".",".",".","#","."],
+    [".","#",".",".",".",".",".","#","."]],
+    [[".", ".", ".", ".", ".", ".", ".", ".", "."],
+     ["#", ".", ".", ".", "#", ".", ".", "#", "."],
+     [".", ".", ".", ".", "#", ".", ".", ".", "."],
+     [".", "#", ".", ".", ".", ".", ".", "#", "."],
+     [".", "#", ".", ".", ".", ".", ".", "#", "."]],
+    [[".", ".", "."],
+     [".", ".", "."],
+     [".", ".", "."]],
+    [[".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+     [".", "#", ".", ".", ".", ".", "#", ".", ".", "."],
+     [".", "#", ".", ".", ".", ".", ".", ".", ".", "."],
+     [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+     [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+     [".", "#", ".", ".", ".", ".", ".", ".", ".", "."],
+     [".", "#", ".", ".", ".", "#", ".", ".", ".", "."],
+     [".", ".", ".", ".", ".", ".", "#", ".", ".", "."],
+     [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+     [".", ".", ".", ".", ".", ".", ".", ".", ".", "."]]
+]
+
+results = [11, -1, 2, 16]
+for labs, res in zip(labyrinths, results):
+    print(solution(labs), "expected:", res)
